483ea379e3a5a3d6bc2728994550ba3aa997ee83
shadon016 <shadon016@gmail.com>
2025-05-16 20:04:35 +0600

    adding deatils page and linking

--- app/(home)/_components/product.jsx
+++ app/(home)/_components/product.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Card from "./card";
+import Link from "next/link";
 
 const Product = () => {
   return (
     <div className="h-[calc(100vh - 1rem)] flex gap-4 flex-wrap">
       {[1, 2, 3, 4, 5, 6, 7, 8].map((item, index) => (
         <div key={index}>
-          <Card />
+          <Link href={`/${index}`}>
+            <Card />
+          </Link>
         </div>
       ))}
     </div>
