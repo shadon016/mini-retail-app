fbdb41fda727ac3a8407f1aa95b860d4d92d8cd1
shadon016 <shadon016@gmail.com>
2025-05-16 19:57:14 +0600

    update product card

--- app/(home)/_components/card.jsx
+++ app/(home)/_components/card.jsx
@@ -1,14 +1,19 @@
 import React from "react";
+import Image from "next/image";
 
 const Card = () => {
   return (
     <div>
-      <div className="bg-white shadow-md rounded-lg p-4">
-        <h2 className="text-xl font-bold mb-2">Card Title</h2>
-        <p className="text-gray-700">This is a simple card component.</p>
-        <button className="mt-4 bg-blue-500 text-white py-2 px-4 rounded">
-          Click Me
-        </button>
+      <div className="bg-slate-900 shadow-md rounded-lg p-4 min-w-[200px] max-w-[300px]">
+        <Image
+          src="/product.jpg"
+          alt="Card Image"
+          width={90}
+          height={60}
+          className="rounded-lg w-full"
+        />
+        <h2 className="py-2">product name</h2>
+        <p className="text-gray-700">product price</p>
       </div>
     </div>
   );
--- app/(home)/_components/product.jsx
+++ app/(home)/_components/product.jsx
@@ -3,7 +3,7 @@ import Card from "./card";
 
 const Product = () => {
   return (
-    <div className="h-[calc(100vh - 1rem)] flex gap-6 flex-wrap">
+    <div className="h-[calc(100vh - 1rem)] flex gap-4 flex-wrap">
       {[1, 2, 3, 4, 5, 6, 7, 8].map((item, index) => (
         <div key={index}>
           <Card />
