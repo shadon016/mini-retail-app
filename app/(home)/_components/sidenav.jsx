4aa1a822708fcee795df2b213b78513fef6c8d85
shadon016 <shadon016@gmail.com>
2025-05-17 10:32:13 +0600

    minor change

--- app/(home)/_components/sidenav.jsx
+++ app/(home)/_components/sidenav.jsx
@@ -6,15 +6,26 @@ const navItem = [
   { id: 2, name: "order" },
   { id: 3, name: "logout" },
 ];
+const authnavItem = [
+  { id: 1, name: "login" },
+  { id: 2, name: "register" },
+];
 const Sidenav = () => {
+  const auth = false;
   return (
     <>
       <div className="sticky top-2 left-0 w-full shadow-2xl bg-slate-900">
-        {navItem?.map((item) => (
-          <div key={item.id}>
-            <p className="text-center py-4">{item.name}</p>
-          </div>
-        ))}
+        {auth
+          ? navItem?.map((item) => (
+              <div key={item.id}>
+                <p className="text-center py-4">{item.name}</p>
+              </div>
+            ))
+          : authnavItem?.map((item) => (
+              <div key={item.id}>
+                <p className="text-center py-4">{item.name}</p>
+              </div>
+            ))}
       </div>
       <div className="sticky top-2 left-0 w-full my-4 shadow-2xl bg-slate-900">
         <Info />
