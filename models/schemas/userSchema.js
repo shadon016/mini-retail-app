b47e4232dc36ef7778a34fc3e36188a18eea93f7
shadon016 <shadon016@gmail.com>
2025-05-19 13:51:29 +0600

    minor fix

--- models/schemas/userSchema.js
+++ models/schemas/userSchema.js
@@ -1,11 +1,13 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true },
-  username: { type: String, required: true },
-  password: { type: String, required: true },
-  address: { type: String },
-  timestamp: { type: Date, default: Date.now },
-});
+const userSchema = new mongoose.Schema(
+  {
+    email: { type: String, required: true, unique: true },
+    username: { type: String, required: true },
+    password: { type: String, required: true },
+    address: { type: String },
+  },
+  { timestamps: true }
+);
 
 export const User = mongoose.models.User ?? mongoose.model("User", userSchema);
