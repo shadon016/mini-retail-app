0d97e001da77406f8ddfccaa05bcce20b8628d54
shadon016 <shadon016@gmail.com>
2025-05-19 18:49:30 +0600

    minor fixes

--- app/(home)/_components/info.jsx
+++ app/(home)/_components/info.jsx
@@ -1,12 +1,15 @@
 import React from "react";
+import Link from "next/link";
 
 const Info = () => {
   return (
     <div>
       <div className="text-center p-4">
-        <h2 className="text-4xl tracking-wide font-extrabold bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 text-transparent bg-clip-text">
-          Zayra
-        </h2>
+        <Link href="/">
+          <h2 className="text-4xl tracking-wide font-extrabold bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 text-transparent bg-clip-text">
+            Zayra
+          </h2>
+        </Link>
         <p className="text-xs my-2">“Smart. Stylish. Seamless Shopping.”</p>
         <p className="text-justify">
           Your go-to destination for trendy finds and everyday essentials — all
--- app/(home)/profile/_components/profile.jsx
+++ app/(home)/profile/_components/profile.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { cookies } from "next/headers";
+import Image from "next/image";
 import { getLoggedInUser } from "@/models/queries/user.js";
 
 const Profile = async () => {
@@ -8,9 +9,18 @@ const Profile = async () => {
   const loggedUser = await getLoggedInUser(userid);
   return (
     <div>
+      <h1 className="border-b-2 mb-4">Persional information</h1>
+      <Image
+        src="/undraw_cool-girl-avatar_fifz.png"
+        height={100}
+        width={100}
+        alt="avatar"
+        className="rounded-full"
+      />
       <h1>{loggedUser?.username}</h1>
       <p>{loggedUser?.email}</p>
-      {/* <p>joined {loggedUser?.timestamp}</p> */}
+      <p>{loggedUser?.address || "No address provided"}</p>
+      <p>joined {loggedUser?.createdAt.toString().slice(0, 15)}</p>
     </div>
   );
 };
