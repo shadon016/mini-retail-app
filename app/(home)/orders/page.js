6f9e4f16bf64742636ae36671163008afc89d961
shadon016 <shadon016@gmail.com>
2025-05-30 18:12:18 +0600

    minor fix

--- app/(home)/orders/page.js
+++ app/(home)/orders/page.js
@@ -1,17 +1,19 @@
 import React from "react";
+import { Order } from "@/models/schemas/orderSchema.js";
 
-const Page = () => {
+const Page = async () => {
+  const orders = await Order.find({}).sort({ createdAt: -1 }).limit(10).lean();
   return (
     <div>
       <h1 className="text-2xl font-bold">Orders</h1>
       <p className="text-gray-600">This is the orders page.</p>
       <div className="mt-4">
         <h2 className="text-xl font-semibold">Order History</h2>
-        <ul className="list-disc pl-5">
-          <li>Order #12345 - Delivered</li>
-          <li>Order #67890 - In Transit</li>
-          <li>Order #54321 - Cancelled</li>
-        </ul>
+        {orders?.map((order) => (
+          <div key={order._id}>
+            <p>{order?.totalPrice}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
